7a446b732e11caf8f0826e0ac2ecbdb3a0620f2a
Peter Ruttkay-Nedecky <ruttkayn@gmail.com>
2020-05-10 20:59:08 +0200

    feat(sever.js): convert to callback

--- ClientApp/src/server.js
+++ ClientApp/src/server.js
@@ -5,15 +5,15 @@ import { renderToString } from 'react-dom/server';
 
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 
-const server = (url) => {
+const server = (cb, url) => {
   const context = {};
   const markup = renderToString(
     <StaticRouter context={context} location={url}>
       <App />
     </StaticRouter>
   );
-
-  return `<!doctype html>
+  
+  cb(null, `<!doctype html>
       <html lang="">
       <head>
           <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -21,8 +21,8 @@ const server = (url) => {
           <title>Welcome to Razzle</title>
           <meta name="viewport" content="width=device-width, initial-scale=1">
           ${assets.client.css
-        ? `<link rel="stylesheet" href="${assets.client.css}">`
-        : ''} 
+      ? `<link rel="stylesheet" href="${assets.client.css}">`
+      : ''} 
           ${process.env.NODE_ENV === 'production'
       ? `<script src="${assets.client.js}" defer></script>`
       : `<script src="${assets.client.js}" defer crossorigin></script>`}
@@ -30,7 +30,7 @@ const server = (url) => {
       <body>
           <div id="root">${markup}</div>
       </body>
-    </html>`
+    </html>`);
 }
 
 export default server;
