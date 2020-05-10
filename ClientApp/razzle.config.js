da4b51ad8238ecd34c7715b459623063af9544ea
Peter Ruttkay-Nedecky <ruttkayn@gmail.com>
2020-05-10 22:02:41 +0200

    chore(razzle): setupt https

--- ClientApp/razzle.config.js
+++ ClientApp/razzle.config.js
@@ -4,6 +4,13 @@ module.exports = {
             config.plugins = config.plugins.filter(p => p.constructor.name !== 'HotModuleReplacementPlugin');
             config.entry = config.entry.filter(e => e !== 'webpack/hot/poll?300');
         }
+        if (target === 'web' && dev) {
+            config.devServer.https = true;
+            config.devServer.pfx = '...';
+            config.devServer.pfxPassphrase = '...';
+            config.output.publicPath=`https://${config.devServer.host}:${config.devServer.port}/`
+        }
+
         return config;
     },
 };
