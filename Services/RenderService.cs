8fde026768ddd02b6c2034a1e1d43f4374beb60f
Peter Ruttkay-Nedecky <ruttkayn@gmail.com>
2020-05-11 14:01:34 +0200

    refactor(RenderService): extract server js path

--- Services/RenderService.cs
+++ Services/RenderService.cs
@@ -12,20 +12,21 @@ namespace AspNetCoreRazzleExample.Services
 
     public class RenderService : IRenderService
     {
+        private static readonly string ServerJsRelPath = "./ClientApp/build/server.js";
         private readonly INodeJSService _nodeJSService;
-        private readonly string _renderJsPath;
+        private readonly string _serverJsPath;
 
         public RenderService(INodeJSService nodeJSService, IWebHostEnvironment webHostEnvironment)
         {
             if (webHostEnvironment is null) throw new System.ArgumentNullException(nameof(webHostEnvironment));
             _nodeJSService = nodeJSService ?? throw new System.ArgumentNullException(nameof(nodeJSService));
-            _renderJsPath = webHostEnvironment.ContentRootFileProvider.GetFileInfo("./ClientApp/build/server.js").PhysicalPath;
+            _serverJsPath = webHostEnvironment.ContentRootFileProvider.GetFileInfo(ServerJsRelPath).PhysicalPath;
         }
         public Task<string> RenderAsync(string url)
         {
             if (string.IsNullOrWhiteSpace(url)) throw new System.ArgumentException($"{nameof(url)} must not be null or empty", nameof(url));
 
-            return _nodeJSService.InvokeFromFileAsync<string>(_renderJsPath, args: new object[] { url });
+            return _nodeJSService.InvokeFromFileAsync<string>(_serverJsPath, args: new object[] { url });
         }
     }
 }
