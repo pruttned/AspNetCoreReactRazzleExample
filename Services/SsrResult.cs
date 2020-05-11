1669fff0246d0b2090a093f403815df9d7d3ed82
Peter Ruttkay-Nedecky <ruttkayn@gmail.com>
2020-05-11 13:54:37 +0200

    refactor(SsrResult): add null checks

--- Services/SsrResult.cs
+++ Services/SsrResult.cs
@@ -9,10 +9,14 @@ namespace AspNetCoreRazzleExample.Services
         private readonly string _url;
         public SsrResult(string url)
         {
+            if (string.IsNullOrWhiteSpace(url)) throw new System.ArgumentException($"{nameof(url)} must not be null or empty", nameof(url));
+
             _url = url;
         }
         public async Task ExecuteResultAsync(ActionContext context)
         {
+            if (context is null) throw new System.ArgumentNullException(nameof(context));
+
             var renderService = context.HttpContext.RequestServices.GetRequiredService<IRenderService>();
             var renderResult = await renderService.RenderAsync(_url);
             var contentResult = new ContentResult
