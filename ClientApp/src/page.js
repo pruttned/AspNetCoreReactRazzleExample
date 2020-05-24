7d7a5df82a10fffedb7fd0a9a010de246a34d78e
Peter Ruttkay-Nedecky <ruttkayn@gmail.com>
2020-05-24 12:22:05 +0200

    fix(page): add handling for location change with the same page component

--- ClientApp/src/page.js
+++ ClientApp/src/page.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { withRouter } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 const page = (WrappedComponent) =>
-  withRouter(({ staticContext, location }) => {
+  ({ staticContext }) => {
+    const location = useLocation();
+
     let initData = null;
     if (staticContext) {
       initData = staticContext.data;
@@ -10,15 +12,20 @@ const page = (WrappedComponent) =>
       initData = window.__ROUTE_DATA__;
       window.__ROUTE_DATA__ = null; // We need to clear __ROUTE_DATA__, so they are not going to be used for another page during client side routing
     }
-    const [pageData, setPageData] = useState(initData);
-    if (!pageData && !staticContext) {
-      fetch(`data${location.pathname}`)
-        .then(r => r.json())
-        .then(setPageData);
+
+    if (!staticContext && !initData) {
+      useEffect(() => {
+        fetch(`data${location.pathname}`)
+          .then(r => r.json())
+          .then(setPageData);
+      }, [location]);
     }
+
+    const [pageData, setPageData] = useState(initData);
+
     return (
       pageData && <WrappedComponent pageData={pageData}></WrappedComponent>
     );
-  });
+  };
 
 export default page;
