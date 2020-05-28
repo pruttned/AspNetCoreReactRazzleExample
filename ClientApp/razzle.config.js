22d16097a1e6f97ecd0c1625a957cefd7ccaaf4c
Peter Ruttkay-Nedecky <ruttkayn@gmail.com>
2020-05-28 09:38:12 +0200

    chore(razzle.config): fix publicPath for ssr

--- ClientApp/razzle.config.js
+++ ClientApp/razzle.config.js
@@ -1,14 +1,16 @@
 module.exports = {
     modify: (config, { target, dev }, webpack) => {
-        if (target === 'node' && dev) {
-            config.plugins = config.plugins.filter(p => p.constructor.name !== 'HotModuleReplacementPlugin');
-            config.entry = config.entry.filter(e => e !== 'webpack/hot/poll?300');
-        }
-        if (target === 'web' && dev) {
-            config.devServer.https = true;
-            config.devServer.pfx = process.env.RAZZLE_PFX;
-            config.devServer.pfxPassphrase = process.env.RAZZLE_PFX_PASSPHRASE;
-            config.output.publicPath=`https://${config.devServer.host}:${config.devServer.port}/`
+        if (dev) {
+            if (target === 'node') {
+                config.plugins = config.plugins.filter(p => p.constructor.name !== 'HotModuleReplacementPlugin');
+                config.entry = config.entry.filter(e => e !== 'webpack/hot/poll?300');
+            }
+            if (target === 'web') {
+                config.devServer.https = true;
+                config.devServer.pfx = process.env.RAZZLE_PFX;
+                config.devServer.pfxPassphrase = process.env.RAZZLE_PFX_PASSPHRASE;
+            }
+            config.output.publicPath = config.output.publicPath.replace('http://', 'https://');
         }
 
         return config;
